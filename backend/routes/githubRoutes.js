d184092e46bdb3f6b89636dbffdd1aaeb367a5d8
Manish Choudhary <me.developer4u@gmail.com>
2026-06-18 17:33:34 +0530

    Delete backend directory

--- backend/routes/githubRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { analyzeProfile } = require("../controllers/githubController");
-
-router.get("/:username", analyzeProfile);
-
-module.exports = router;
